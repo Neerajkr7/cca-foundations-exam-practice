b0d42c9920e43c15730d3aebf743cefb592f65b9
neerajwdtech <neeraj@webdura.tech>
2026-04-13 14:20:40 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Change 'cca-foundations-exam' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/cca-foundations-exam/',
-})
+  base: "/cca-foundations-exam-practice/",
+});
